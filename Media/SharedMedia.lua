45cdba41959f51bd1a80e91d483f272bc9c02634
Robin <robin-nilsson@hotmail.com>
2025-08-17 12:20:32 +0200

    feat: Update sounds

--- Media/SharedMedia.lua
+++ Media/SharedMedia.lua
@@ -24,32 +24,34 @@ end
 LSM:Register(MediaType_SOUND, "MSBT - Cooldown", [[Interface\Addons\LauringUI\Media\Sounds\MSBT - Cooldown.ogg]])
 
 local sounds = {
-    "Lauring - 1", "Lauring - 2", "Lauring - 3", "Lauring - 4", "Lauring - 5",
-    "Lauring - 6", "Lauring - 7", "Lauring - 8", "Lauring - 9", "Lauring - Add",
-    "Lauring - Adds", "Lauring - AMS", "Lauring - AOE", "Lauring - Avoid", "Lauring - Beam",
-    "Lauring - Big Add", "Lauring - Black", "Lauring - Bloodlust", "Lauring - Blue", "Lauring - Bomb",
-    "Lauring - Break", "Lauring - Breath", "Lauring - Buff", "Lauring - CC", "Lauring - CCd",
-    "Lauring - Charge", "Lauring - Clear", "Lauring - Dance inc", "Lauring - Dance", "Lauring - Dark",
-    "Lauring - Debuff", "Lauring - Defensive", "Lauring - Dispell", "Lauring - Dodge inc", "Lauring - Dodge",
-    "Lauring - Dont Move", "Lauring - Dot", "Lauring - Down", "Lauring - Enrage Inc", "Lauring - Enrage",
-    "Lauring - Execute", "Lauring - Fear", "Lauring - Feared", "Lauring - Fixate", "Lauring - Frontal",
-    "Lauring - Gloves", "Lauring - Green", "Lauring - Healthstone", "Lauring - Hide", "Lauring - High Stacks",
-    "Lauring - Immune", "Lauring - In", "Lauring - Inside", "Lauring - Intermission", "Lauring - Interrupt",
-    "Lauring - Jump", "Lauring - Kick", "Lauring - Kite", "Lauring - Left", "Lauring - Light",
-    "Lauring - LoS", "Lauring - MC", "Lauring - Middle", "Lauring - Move", "Lauring - Next",
-    "Lauring - Nitro", "Lauring - Nuke", "Lauring - On you", "Lauring - Orange", "Lauring - Orb",
-    "Lauring - Orbs", "Lauring - Out", "Lauring - Outrange", "Lauring - Outside", "Lauring - Personal",
-    "Lauring - Phase change", "Lauring - Pot", "Lauring - Purple", "Lauring - Push", "Lauring - Racial",
-    "Lauring - Ready", "Lauring - Red", "Lauring - Right", "Lauring - Rooted", "Lauring - Shield",
-    "Lauring - Silenced", "Lauring - Smash inc", "Lauring - Smash", "Lauring - Soak", "Lauring - Spread",
-    "Lauring - Stack", "Lauring - Stay", "Lauring - Stop", "Lauring - Stopcast", "Lauring - Stunned",
-    "Lauring - Switch", "Lauring - Taunt", "Lauring - Throw", "Lauring - Trap", "Lauring - Trinket",
-    "Lauring - Turn", "Lauring - Up", "Lauring - Watch Feet", "Lauring - White", "Lauring - Yellow"
+    "1", "2", "3", "4", "5",
+    "6", "7", "8", "9", "Add",
+    "Adds", "AMS", "AOE", "Avoid", "Banner",
+    "Beam", "Big Add", "Black", "Bloodlust", "Blue",
+    "Bomb", "Break", "Breath", "Buff", "CC",
+    "CCd", "Charge", "Clear", "Dance", "Dance inc",
+    "Dark", "Debuff", "Defensive", "Disoriented", "Dispell",
+    "Dodge", "Dodge inc", "Dont Move", "Dot", "Down",
+    "Enrage", "Enrage Inc", "Execute", "Fear", "Feared",
+    "Fixate", "Frontal", "Gloves", "Green", "Healthstone",
+    "Hide", "High Stacks", "Immune", "In", "Inside",
+    "Intermission", "Interrupt", "Jump", "Kick", "Kite",
+    "Left", "Light", "LoS", "MC", "Middle",
+    "Move", "Next", "Nitro", "Nuke", "On you",
+    "Orange", "Orb", "Orbs", "Out", "Outrange",
+    "Outside", "Personal", "Phase change", "Pot", "Proc",
+    "Purple", "Push", "Racial", "Ready", "Red",
+    "Right", "Rooted", "Shield", "Silenced", "Smash",
+    "Smash inc", "Soak", "Spread", "Stack", "Stay",
+    "Stop", "Stopcast", "Stunned", "Switch", "Taunt",
+    "Throw", "Totem", "Trap", "Trinket", "Turn",
+    "Up", "Watch Feet", "Weapon", "White", "Yellow"
 }
 
 for _, sound in ipairs(sounds) do
-    local label = "|cff8033b3" .. sound .. "|r"
-    local file = [[Interface\Addons\LauringUI\Media\Sounds\]] .. sound .. ".ogg"
+    local name = "Lauring - " .. sound
+    local label = "|cff8033b3" .. name .. "|r"
+    local file = [[Interface\Addons\LauringUI\Media\Sounds\]] .. name .. ".mp3"
     LSM:Register(MediaType_SOUND, label, file)
 end
 
